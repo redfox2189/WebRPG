71daad9e50648212fa791976fef0f1588d9e30b2
Miles <miles.christiansen@southeasttech.edu>
2026-03-25 13:30:21 -0500

    started java

---
+++ Game.js
@@ -0,0 +1,23 @@
+const $ = (selector) => document.querySelector(selector);
+
+const dom = {
+    Message: $('#Message'),
+    Monster_Side: $('#Monster_Side'),
+    Monster_Name: $('#Monster_Name'),
+    Monster_Image: $('#Monster_Image'),
+    Monster_Health: $('#Monster_Health'),
+    Start: $('#Start'),
+    Combat: $('#Combat'),
+    Shop: $('#Shop'),
+    Bag: $('#Bag'),
+    Player_Side: $('#Player_Side'),
+    Health: $('#Health'),
+    Attack: $('#Attack'),
+    Defence: $('#Defence'),
+    Gold: $('#Gold'),
+    Bag: $('#Bag'),
+    Item_List: $('#Item_List'),
+    Item_Use: $('#Item_Use'),
+    Item_Drop: $('#Item_Drop'),
+}
+
